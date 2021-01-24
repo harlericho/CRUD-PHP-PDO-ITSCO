1402b547c46d788687191f4011adad34e7365a63
charlie <harlericho@gmail.com>
2021-01-24 18:53:09 -0500

    modificacion db

--- datos/conexion.php
+++ datos/conexion.php
@@ -1,9 +1,7 @@
 <?php
 try {
-    $pdo = new PDO('mysql:dbname=sextonocturna;host=localhost', 'root', '');
+    $pdo = new PDO('mysql:dbname=db_sextonocturna;host=localhost', 'charlie', 'charlie');
     //echo "<script>alert('Conectado a la base');</script>";
 } catch (Exception $exc) {
-    die('Error: '.$exc->getMessage());
+    die('Error: ' . $exc->getMessage());
 }
-
-
